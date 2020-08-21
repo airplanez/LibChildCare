82b572dc050db6afc68aebc81c693458b146fc6c
airplanez <airplanez@gmail.com>
2020-08-21 13:49:47 +0900

    version 1.0.1

--- childcare/src/main/java/net/airplanez/android/lib/childcare/ChildCare.kt
+++ childcare/src/main/java/net/airplanez/android/lib/childcare/ChildCare.kt
@@ -3,7 +3,7 @@ package net.airplanez.android.lib.childcare
 class ChildCare {
     companion object {
         fun getVersion(): String {
-            return "v1.0.0"
+            return "v1.0.1"
         }
     }
 }
